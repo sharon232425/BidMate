820b5f1c2078f69cd218509b4afbcbc693453ea2
sharon232323 <daisysharon232@gmail.com>
2026-06-16 15:21:27 +0530

    Fix register crash render final

--- app.py
+++ app.py
@@ -412,5 +412,10 @@ def chat(item_id):
         item=item,
         messages=messages
     )
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
