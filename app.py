7976e6caa83ff098279123b745881974d0f9b0af
sharon232323 <daisysharon232@gmail.com>
2026-06-17 10:49:33 +0530

    Fix barter model mismatch database reset

--- app.py
+++ app.py
@@ -429,8 +429,8 @@ def chat(item_id):
         messages=messages
     )
 with app.app_context():
+    db.drop_all()
     db.create_all()
-
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
